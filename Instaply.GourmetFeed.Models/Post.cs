2a4eecfa540c0a2a5d6262ecad2b38187accf5a5
floriandubois <ap.floriandubois@gmail.com>
2014-09-21 15:50:27 -0700

    Added the models: - User - Posts

---
+++ Instaply.GourmetFeed.Models/Post.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Instaply.GourmetFeed.Models
+{
+    public class Post
+    {
+        /// <summary>
+        /// Represents the root URL of the images
+        /// </summary>
+        public string ImagesRootUrl { get; set; }
+        /// <summary>
+        /// List of posts
+        /// </summary>
+        public List<PostDetails> Posts { get; set; }
+    }
+
+    public class PostDetails
+    {
+        /// <summary>
+        /// Title of the post
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// Description of the post
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// Author of the post
+        /// </summary>
+        public string Author { get; set; }
+        /// <summary>
+        /// Creation time
+        /// </summary>
+        public DateTime CreatedTime { get; set; }
+        /// <summary>
+        /// UID of the post
+        /// </summary>
+        public Guid ResourceIdentifier { get; set; }
+        /// <summary>
+        /// URL of the photos
+        /// </summary>
+        public string[] PhotoUrls { get; set; }
+        /// <summary>
+        /// Photos count in post
+        /// </summary>
+        public int Photos { get; set; }
+        public string PhotoUrl { get { return PhotoUrls[0]; } }
+    }
+}
---
+++ Instaply.GourmetFeed.Models/User.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Instaply.GourmetFeed.Models
+{
+    public class User
+    {
+        /// <summary>
+        /// Email of the user
+        /// </summary>
+        public string Email { get; set; }
+        /// <summary>
+        /// Password of the user
+        /// </summary>
+        public string Password { get; set; }
+        /// <summary>
+        /// UID of the user
+        /// </summary>
+        public string IdentificationService { get; set; }
+        /// <summary>
+        /// First name of the user
+        /// </summary>
+        public string FirstName { get; set; }
+        /// <summary>
+        /// Last name of the user
+        /// </summary>
+        public string LastName { get; set; }
+        /// <summary>
+        /// Access token
+        /// </summary>
+        public string AccessToken { get; set; }
+    }
+}
