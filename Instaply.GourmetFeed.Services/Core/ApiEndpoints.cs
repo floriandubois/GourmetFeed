64eaff34bb1498cf30e47838ff836548bd44febd
floriandubois <ap.floriandubois@gmail.com>
2014-09-21 15:43:54 -0700

    Adding api endpoints

---
+++ Instaply.GourmetFeed.Services/Core/ApiEndpoints.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Instaply.GourmetFeed.Services.Core
+{
+    /// <summary>
+    /// Endpoints for contacting service
+    /// </summary>
+    internal static class ApiEndpoints
+    {
+        #region Api
+
+        /// <summary>
+        /// Base URL of the service
+        /// </summary>
+        public const string BaseUrl = "http://devapi.gourmetfeed.com/";
+
+        /// <summary>
+        /// Current service version
+        /// </summary>
+        public const string Version = "/v1";
+
+        #endregion
+
+        #region User
+
+        /// <summary>
+        /// Login extension
+        /// </summary>
+        public const string Login = "/login";
+
+        /// <summary>
+        /// Create user extension
+        /// </summary>
+        public const string CreateUser = "/login";
+
+        #endregion
+
+        #region Posts
+
+        /// <summary>
+        /// Get posts extension
+        /// </summary>
+        public const string GetPosts = "/posts";
+
+        #endregion
+    }
+}
