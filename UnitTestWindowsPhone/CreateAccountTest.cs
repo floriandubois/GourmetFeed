a694769ecc4ff5c0f24661bbfedd0c2dd93a5c62
floriandubois <ap.floriandubois@gmail.com>
2014-09-21 18:11:44 -0700

    Added tests for login

--- UnitTestWindowsPhone/CreateAccountTest.cs
+++ UnitTestWindowsPhone/CreateAccountTest.cs
@@ -118,4 +118,75 @@ namespace UnitTestWindowsPhone
             Assert.AreEqual(eValidationValues.LastNameEmpty, result, "Login: Validate fields with empty last name success");
         }
     }
+
+    /// <summary>
+    /// Test the login feature
+    /// </summary>
+    [TestClass]
+    public class LoginTest
+    {
+        /// <summary>
+        /// Test the validating method to ensure it returns valid when all the fields are correctly filled
+        /// </summary>
+        [TestMethod]
+        public void ValidateFieldsTest_Valid()
+        {
+            User user = new User
+            {
+                Email = "test@test.com",
+                Password = "password"
+            };
+
+            var result = user.ValidateLogin();
+            Assert.AreEqual(eValidationValues.Valid, result, "Login: Validate fields with correct values success");
+        }
+
+        /// <summary>
+        /// Test the validating method to ensure it returns EmailInvalid when the Email address is invalid
+        /// </summary>
+        [TestMethod]
+        public void ValidateFieldsTest_UnvalidEmail()
+        {
+            User user = new User
+            {
+                Email = "test",
+                Password = "password"
+            };
+
+            var result = user.ValidateLogin();
+            Assert.AreEqual(eValidationValues.EmailInvalid, result, "Login: Validate fields with invalid email success");
+        }
+
+        /// <summary>
+        /// Test the validating method to ensure it returns EmailAddressEmpty when the email address is empty
+        /// </summary>
+        [TestMethod]
+        public void ValidateFieldsTest_EmptyEmail()
+        {
+            User user = new User
+            {
+                Email = "",
+                Password = "password"
+            };
+
+            var result = user.ValidateLogin();
+            Assert.AreEqual(eValidationValues.EmailAddressEmpty, result, "Login: Validate fields with empty email success");
+        }
+
+        /// <summary>
+        /// Test the validating method to ensure it returns PasswordEmpty when the Password is empty
+        /// </summary>
+        [TestMethod]
+        public void ValidateFieldsTest_EmptyPassword()
+        {
+            User user = new User
+            {
+                Email = "test@test.com",
+                Password = ""
+            };
+
+            var result = user.ValidateLogin();
+            Assert.AreEqual(eValidationValues.PasswordEmpty, result, "Login: Validate fields with empty password success");
+        }
+    }
 }
